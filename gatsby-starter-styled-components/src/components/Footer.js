0d85b1ac618e9d0aa33884aab9de16c608f605a2
Michael Cuccia <michael.cuccia@us4desm0fg4n0md.home>
2021-07-13 09:25:36 -0400

    final touches before deployment

--- gatsby-starter-styled-components/src/components/Footer.js
+++ gatsby-starter-styled-components/src/components/Footer.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
-import imageFooter from '../images/assets/image-footer.jpg';
+import imageFooter from '../images/assets/footerImage.png';
 import Button from './Button';
 
 const MainContainer = styled.div`
     background-image: url(${imageFooter});
     background-repeat: no-repeat;
     background-position: center;
+    background-size: cover;
     width: 100%;
     height: 308px;
     margin-top: 40px;  
+
+    @media (max-width: 765px) {
+        
+    }
 `;
 
 const TextContainer = styled.div`
     display: flex;    
     justify-content: space-around;
-    padding-top: 100px;  
+    padding-top: 125px; 
+    
+    @media (max-width: 765px) {
+        flex-direction: column;
+        align-items: center;
+        padding-top: 25px; 
+    }
 
     p {
         color: #FAFAFA;  
@@ -25,12 +36,37 @@ const TextContainer = styled.div`
         font-size: 40px;
         height: 88px;
         width: 350px;
+
+        @media (max-width: 1000px) {
+            font-size: 34px;
+            height: 78px;
+            width: 250px;
+        }
+
+        @media (max-width: 765px) {
+            font-size: 28px;
+            text-align: center;
+            padding-top: 10px;
+        }
     }
 
     .pTwo {
         font-size: 18px;
         height: 88px;
         width: 350px;
+
+        @media (max-width: 1000px) {
+            font-size: 16px;
+            height: 78px;
+            width: 250px;
+        }
+
+        @media (max-width: 765px) {
+            font-size: 14px;
+            text-align: center;
+            padding-top: 15px;
+            margin-bottom: 25px;
+        }
     }
 
 `;
